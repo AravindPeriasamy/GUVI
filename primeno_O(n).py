5bcbb5a08dbcbf6ee8555689242dc4b4b8419b96
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-01-09 16:19:27 +0500

    Create primeno_O(n).py

---
+++ primeno_O(n).py
@@ -0,0 +1,13 @@
+start, end = map(int,input().split(" "))
+prime = [True for i in range(end+1)] 
+p = 2
+count = 0
+while (p * p <= end): 
+    if (prime[p] == True): 
+        for i in range(p * p, end+1, p): 
+            prime[i] = False
+    p += 1
+for p in range(start, end): 
+        if prime[p]: 
+            count+=1
+print(count)
