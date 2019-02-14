63dc57afea81e83e3df663063eaae403ae95a0d6
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-02-14 14:31:03 +0530

    Update pro40.py

--- pro40.py
+++ pro40.py
@@ -1,12 +1,17 @@
 a = input()
 string = ['d','h','o','n','i']
 count = [0,0,0,0,0]
+exit = False
 for i in a:
 	if i in string:
 		count[string.index(i)] += 1
+	if i not in string:
+		print("no")
+		exit = True
 for i in count:
-	if i!=1:
+	if i!=1 and exit!=True:
 		print("no")
 		break
 else:
-	print("yes")
+	if exit!=True:
+		print("yes")
