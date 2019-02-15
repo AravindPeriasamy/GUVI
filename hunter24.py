c3964b89ac434fa7f1d62a1b3ff150e1d19daea4
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-02-15 15:45:51 +0530

    Create hunter24.py

---
+++ hunter24.py
@@ -0,0 +1,13 @@
+a,b = map(int,input().split(" "))
+flag = False
+list1 = list(map(int,input().split(" ")))
+for i in range(0,(a-1)):
+	for j in range((i+1),a):
+		if list1[i] + list1[j] == b:
+			print("YES")
+			flag = True
+			break
+		if flag == True:
+			break
+if flag == False:
+	print("NO")
