57ac8a3ba59b1b2d0bca293a0d3bb4dd802d533b
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-02-16 18:45:17 +0530

    Create pro12.py

---
+++ pro12.py
@@ -0,0 +1,11 @@
+a, b = map(int,input().split(" "))
+list1 = list(map(int,input().split(" ")))
+list1.insert(0,0)
+one = []
+two = []
+for i in range(b):
+	x, y = map(int,input().split(" "))
+	one.append(x)
+	two.append(y)
+for i in range(b):
+	print(sum(list1[one[i]:(two[i]+1)]))
