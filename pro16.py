78177384bcf220a7ef71457ee782826dfd4e9a88
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-02-16 19:37:12 +0530

    Create pro16.py

---
+++ pro16.py
@@ -0,0 +1,11 @@
+a = int(input())
+list1 = [int(i) for i in input().split()]
+list1.sort()
+increment = 1
+candies = 0
+list1.insert(0,list1[0])
+for i in range(1,len(list1)):
+	if list1[i]>list1[i-1]:
+		increment += 1
+	candies += increment
+print(candies)
