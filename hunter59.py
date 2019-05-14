c84d6a0773dd7acb440a93813e303cdfee174dfd
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-14 12:09:35 +0530

    Update hunter59.py

--- hunter59.py
+++ hunter59.py
@@ -1,7 +1,7 @@
 x = int(input())
 list1 = list(map(int,input().split(" ")))
-list2 = list(map(int,input().split(" ")))
+list4= list(map(int,input().split(" ")))
 list3 = []
 for i in range(x):
-	list3.append(list1[i]+list2[i])
+	list3.append(list1[i]+list4[i])
 print(*list3)
