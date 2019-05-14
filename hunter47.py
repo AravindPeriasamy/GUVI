f2564fadf3c86c81f29e32671ee43348655bff85
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-14 11:02:42 +0530

    Update hunter47.py

--- hunter47.py
+++ hunter47.py
@@ -1,7 +1,10 @@
 list1 = list(map(str,input().split(" ")))
 output = ""
-for i in range(0,(len(list1)-1)):
-	if list1[i] != "":
-		output += (list1[i].strip()) + " "
-output += (list1[(len(list1)-1)].strip())
-print(output)
+if len((list1)) == 1:
+	print(*list1)
+else:
+	for i in range(0,(len(list1)-1)):
+		if list1[i] != "":
+			output += (list1[i].strip()) + " "
+	output += (list1[(len(list1)-1)].strip())
+	print(output)
