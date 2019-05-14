460a6f1a4861f444ef32ad7b37fc12ce60e2554d
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-14 11:58:34 +0530

    Update hunter58.py

--- hunter58.py
+++ hunter58.py
@@ -2,6 +2,7 @@ x, y = map(int, input().split())
 list1 = list(map(int,input().split(" ")))
 count = 0
 for i in range(0,(x-1)):
-	if abs(list1[i]-list1[i+1]) == y:
-		count+=1
+	for j in range(i+1,x):
+		if abs(list1[i]-list1[j]) == y:
+			count+=1
 print(count)
