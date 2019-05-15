62b1a6adce74e4e91c4ceda40afeae1a63c84a24
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-15 15:34:16 +0530

    Create hunter95.py

---
+++ hunter95.py
@@ -0,0 +1,16 @@
+end = int(input())
+start = 1
+prime = [True for i in range(end+1)] 
+p = 2
+outlist = []
+while (p * p <= end): 
+    if (prime[p] == True): 
+        for i in range(p * p, end+1, p): 
+            prime[i] = False
+    p += 1
+for p in range(start, end): 
+        if prime[p] and p!=1: 
+            outlist.append(p)
+if not outlist:
+	outlist.append(0)
+print(*outlist)
