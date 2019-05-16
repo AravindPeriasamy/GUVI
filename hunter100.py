3447a34982892ca567d4d7bd75b79160f4a3cdc0
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-16 22:25:30 +0530

    Create hunter100.py

---
+++ hunter100.py
@@ -0,0 +1,20 @@
+end = int(input())
+sta = 1
+prime = [True for i in range(end+1)] 
+p = 2
+out = []
+outlist = []
+while (p * p <= end): 
+    if (prime[p] == True): 
+        for i in range(p * p, end+1, p): 
+            prime[i] = False
+    p += 1
+for p in range(sta, end): 
+        if prime[p] and p!=1: 
+            outlist.append(p)
+for i in range(0,(len(outlist)+1)):
+	for j in range(i+1,len(outlist)):
+		if outlist[i]+outlist[j] == end:
+			out.append(outlist[i])
+			out.append(outlist[j])
+print(*out)
