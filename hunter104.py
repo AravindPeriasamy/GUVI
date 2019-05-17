832ff37176bf9fd66aa976d400f2d07726463b0b
Aravind P <40619700+AravindPeriasamy@users.noreply.github.com>
2019-05-17 09:34:39 +0530

    Update hunter104.py

--- hunter104.py
+++ hunter104.py
@@ -1,8 +1,11 @@
 x = list(input())
 list1 = []
 out = 0
-for i in x:
-	list1.append(int(i))
-for i in range(len(list1)):
-	out += sum(list1[0:(i+1)])
+if x == 1:
+	out = 1
+else:
+	for i in x:
+		list1.append(int(i))
+	for i in range(len(list1)):
+		out += sum(list1[0:(i+1)])
 print(out)
